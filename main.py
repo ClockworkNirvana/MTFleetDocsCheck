420a266b68567e7cde7056c7df3ec9d1eea35ca4
ClockworkNirvana <42644524+ClockworkNirvana@users.noreply.github.com>
2024-01-21 13:15:41 +0000

    added ipsfBOS and AOS tests, matching tests and too early/late tests

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import csv
 from dataclass import Rawdata
 from tests import *
+from datetime import date
 
+#edit these only
 servicingFile = "download-13_24_56.csv"
 tripsFile = "download-13_24_49.csv"
+start = date(2024, 1, 15)
+end = date(2024, 1, 19)
 
 data = []
 
+#read out the servicing file
 with open(servicingFile, 'r') as f:
     reader = csv.reader(f)
     next(reader)
@@ -15,6 +20,7 @@ with open(servicingFile, 'r') as f:
         toAdd.build_servicing(row)
         data.append(toAdd)
 
+#read out the trips file
 with open(tripsFile, 'r') as f:
     reader = csv.reader(f)
     next(reader)
@@ -25,12 +31,49 @@ with open(tripsFile, 'r') as f:
         toAdd = Rawdata()
         toAdd.build_trip(row)
         data.append(toAdd)
+        
+#del old data
+data = [x for x in data if start <= x.timeStart.date() <= end]
 
+# sort data
+data.sort(key=lambda x: x.timeStart)
+
+#split data into days
+days = {}
 for event in data:
-    time_test(event)
-    tooLong_test(event)
-    A5_test(event)
-    beforeAfter_test(event)
-    WPT_test(event)
-    MPT_test(event)
-    speed_test(event)
+    days.setdefault(event.timeStart.toordinal(), []).append(event)
+days = [days.get(day, []) for day in range(min(days), max(days)+1)]
+days = [x for x in days if x is not []]
+
+AROS = [59301,59260,59217,59133,59412,59087,59232,59022,59038]
+OUV = [34397,34398,34399]
+
+#daily checks
+for day in days:
+    morn_BOS = {x:False for x in AROS}
+    night_AOS = {x:False for x in AROS}
+    counter = {x:[False, False] for x in AROS+OUV}
+    for event in day:
+        if event.purpose == 'BOS':
+            counter[int(event.vehicle)] = [True, counter[int(event.vehicle)][1]]
+        elif event.purpose == 'AOS':
+            counter[int(event.vehicle)] = [counter[int(event.vehicle)][0], True]
+            
+        time_test(event)
+        tooLong_test(event)
+        A5_test(event)
+        beforeAfter_test(event)
+        WPT_test(event)
+        MPT_test(event)
+        speed_test(event)
+        early_late_test(event)
+        # for IPSF veh
+        if int(event.vehicle) in AROS:
+            morn_BOS, counter = ipsfBos_test(event, morn_BOS, counter)
+            night_AOS, counter = ipsfAos_test(event, night_AOS, morn_BOS, counter)
+            
+    matching_test(counter, day[0].timeStart.date())
+        
+#    chronoCheckVehicle_test(event)
+#    chronoCheckDriver_test(event)
+#    meter_test(event)
--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import timedelta, time
 from dataclass import Rawdata
     
 def chronoCheckVehicle_test(event: Rawdata):
@@ -10,14 +10,41 @@ def chronoCheckDriver_test(event: Rawdata):
 def meter_test(event: Rawdata):
     pass
 
-def ipsfBos_test(event: Rawdata):
-    pass
+def ipsfBos_test(event: Rawdata, BOS: dict[int, bool], counter: dict[int, list[bool]]):
+    if event.purpose == 'BOS' and event.timeStart.time() < time(7, 0):
+        if BOS[int(event.vehicle)] is False:
+            BOS[int(event.vehicle)] = True
+        else:
+            print('Error in {}, AM IPSF performed twice for {}'.format(
+                event.id, event.vehicle))
+        counter[int(event.vehicle)] = [True, counter[int(event.vehicle)][1]]
+    return BOS, counter
 
-def ipsfAos_test(event: Rawdata):
-    pass
+def ipsfAos_test(event: Rawdata, AOS: dict[int, bool], 
+                 BOS: dict[int, bool], counter: dict[int, list[bool]]):   
+    if event.purpose == 'AOS' and event.timeStart.time() > time(19, 0):
+        if BOS[int(event.vehicle)]:
+            if not AOS[int(event.vehicle)]:
+                AOS[int(event.vehicle)] = True
+            else:
+                print('Error in {}, PM IPSF performed twice for {}'.format(
+                    event.id, event.vehicle))
+        else:
+            print('Possible error in {}, AOS performed for {} after 7pm when no BOS done'
+                  .format(event.id, event.vehicle))
+        counter[int(event.vehicle)] = [counter[int(event.vehicle)][0], True]
+    return AOS, counter
     
-def matching_test(event: Rawdata):
-    pass
+def matching_test(counter: dict[int, list[bool]], day):
+    for veh, outstanding in counter.items():
+        match outstanding:
+            case [True, False]:
+                print('Error on {}, AOS not performed for {}'.format(
+                    day.isoformat(), veh))
+            case [False, True]:
+                print('Error on {}, BOS not performed for {}'.format(
+                    day.isoformat(), veh))
+            
 
 def WPT_test(event: Rawdata):
     flag = False
@@ -62,3 +89,9 @@ def MPT_test(event: Rawdata):
 def speed_test(event: Rawdata):
     if event.purpose not in ['BOS','AOS','AHS','POL'] and (int(event.dist)/event.duration.total_seconds()) > 0.0166:
         print("Error in {}, average speed more than 60km/hr".format(event.id))
+
+def early_late_test(event: Rawdata):
+    if event.timeStart.time() < time(6, 30):
+        print("Error in {}, {} conducted before 6.30am".format(event.id, event.purpose))
+    if event.timeEnd.time() > time(19, 30):
+        print("Error in {}, {} conducted after 7.30pm".format(event.id, event.purpose))
